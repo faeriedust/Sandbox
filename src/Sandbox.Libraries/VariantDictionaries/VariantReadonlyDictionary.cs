fedf6dc04a5955ac0367738e2fa24e1eab45bf8d
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2016-12-23 12:38:01 +0900

    added variant dictionary

---
+++ src/Sandbox.Libraries/VariantDictionaries/VariantReadonlyDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sandbox.Libraries.VariantDictionaries {
+  public class VariantReadonlyDictionary<TKey, TValue> : IVariantReadonlyDictionary<TKey, TValue> {
+    private readonly IDictionary<TKey, TValue> _dictionary;
+    public VariantReadonlyDictionary(IDictionary<TKey, TValue> dictionary) {
+      _dictionary = dictionary;
+    }
+    public TValue this[TKey key] => _dictionary[key];
+    public int Count => _dictionary.Count;
+    public IEnumerable<TKey> Keys => _dictionary.Keys;
+    public IEnumerable<TValue> Values => _dictionary.Values;
+    public bool ContainsKey(TKey key) => _dictionary.ContainsKey(key);
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => _dictionary.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable) _dictionary).GetEnumerator();
+  }
+}
