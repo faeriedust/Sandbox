34cb8edd87b28160c464fc72e08f5cdfab6562d3
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2016-12-23 10:19:01 +0900

    added test project

---
+++ test/Sandbox.Test/RandomTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sandbox.Test {
+  public class RandomTests {
+    [Fact]
+    public void TwoPlusTwoIsFour() {
+      Assert.Equal(4, 2 + 2);
+    }
+  }
+}
