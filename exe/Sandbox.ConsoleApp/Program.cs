9769704ba4709f6dbf8bb8a2af613c336d48524c
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2016-12-23 11:37:25 +0900

    added resources project

--- exe/Sandbox.ConsoleApp/Program.cs
+++ exe/Sandbox.ConsoleApp/Program.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Sandbox.ConsoleApp.Configuration;
+using Sandbox.Resources;
 
 namespace Sandbox.ConsoleApp {
   public class Program {
     public static void Main(string[] args) {
       var config = SandboxConfiguration.ConfigurationRoot;
-      Console.WriteLine("Hello world!");
+      Console.WriteLine(SandboxResources.HelloWorld);
       Console.ReadLine();
     }
   }
