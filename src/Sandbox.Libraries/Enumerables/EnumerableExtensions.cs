c2ffffe1a3f6d4c7610d07616c5000d241fac7c2
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2016-12-23 12:17:54 +0900

    added a couple extensions off ienumerable for setequality

---
+++ src/Sandbox.Libraries/Enumerables/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sandbox.Libraries.Sets {
+  public static class EnumerableExtensions {
+    public static bool SetEqual<T>(this IEnumerable<T> first, IEnumerable<T> second) {
+      return first.ContainsSet(second) && second.ContainsSet(first);
+    }
+    public static bool ContainsSet<T>(this IEnumerable<T> source, IEnumerable<T> innerSet) {
+      var sourceSet = new HashSet<T>(source);
+      return innerSet.All(sourceSet.Add);
+    }
+  }
+}
