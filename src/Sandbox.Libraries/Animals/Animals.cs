5086eef35c7f1bb55bafbc2ef24b2ff0cc7c10f8
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2016-12-23 12:11:45 +0900

    added animals

---
+++ src/Sandbox.Libraries/Animals/Animals.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sandbox.Libraries.Animals {
+  public abstract class Animal { }
+  public class Lion : Animal { }
+  public class Giraffe : Animal { }
+}
