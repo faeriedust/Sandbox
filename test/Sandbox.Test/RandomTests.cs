6a4392c9938d67e8f69ee4b7ccae57e37d55ecd7
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2016-12-31 03:53:28 +0900

    added a theory test

---
+++ test/Sandbox.Test/RandomTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sandbox.Test {
+  public class RandomTests {
+    [Theory]
+    [InlineData(12, 4, 3)]
+    [InlineData(12, 3, 4)]
+    [InlineData(12, 6, 2)]
+    public void DivisionTest(int n, int d, int r) {
+      Assert.Equal(r, n / d);
+    }
+  }
+}
